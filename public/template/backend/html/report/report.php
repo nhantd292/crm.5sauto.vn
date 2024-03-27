9023b99f10c5ccaf30e276b869a970829f0de822
nhantd292 <nhantd.292@gmail.com>
2024-03-27 07:53:15 +0700

    thay đổi tên báo cáo chia số

--- public/template/backend/html/report/report.php
+++ public/template/backend/html/report/report.php
@@ -119,7 +119,7 @@ $is_accounting = true;
                         <a href="<?php echo $this->url('routeReport/default', array('controller' => 'index', 'action' => 'index', 'id' => 'sale', 'code' => 'sale3'));?>"><i class="fa fa-dot-circle-o"></i> Báo cáo doanh thu sale</a>
                     </li>
                     <li>
-                        <a href="<?php echo $this->url('routeReport/default', array('controller' => 'index', 'action' => 'index', 'id' => 'sale', 'code' => 'share'));?>"><i class="fa fa-dot-circle-o"></i> Báo cáo chia số</a>
+                        <a href="<?php echo $this->url('routeReport/default', array('controller' => 'index', 'action' => 'index', 'id' => 'sale', 'code' => 'share'));?>"><i class="fa fa-dot-circle-o"></i> Báo cáo nhậ số - Tỉ lệ chốt</a>
                     </li>
 <!--                    <li>-->
 <!--                        <a href="--><?php //echo $this->url('routeReport/default', array('controller' => 'index', 'action' => 'index', 'id' => 'sale', 'code' => 'sale-store'));?><!--"><i class="fa fa-dot-circle-o"></i> Báo cáo doanh thu cửa hàng</a>-->
