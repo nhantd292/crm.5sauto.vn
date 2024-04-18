01d757d37bb499767648d55cf7ea0db0545384ef
nhantd292 <nhantd.292@gmail.com>
2024-04-18 07:09:48 +0700

    fix lỗi gid

--- library/ZendX/Functions/Gid.php
+++ library/ZendX/Functions/Gid.php
@@ -5,7 +5,8 @@ class Gid {
     protected $_string;
     
     public function __construct($string = null) {
-        $this->_string = $this->_string ? $this->_string : '0123456789abcde0123456789fghi0123456789jklm0123456789nopqrst0123456789uvwxyz0123456789';
+//        $this->_string = $this->_string ? $this->_string : '0123456789abcde0123456789fghi0123456789jklm0123456789nopqrst0123456789uvwxyz0123456789';
+        $this->_string = $this->_string ? $this->_string : 'kjasdofguas0rtlkwjer0q8r9udg9082q40trusqoidufg99a8syerqy93rqwydfy872q8734r59uqwefglakgt9w8e5ytkjdofgu0sd';
     }
     
     public function getId() {
