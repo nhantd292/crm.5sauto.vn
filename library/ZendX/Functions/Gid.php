c8bab54578699cb5e2beb27283faea5cecb62106
nhantd292 <nhantd.292@gmail.com>
2024-04-18 07:15:32 +0700

    fix lỗi hàm getId

--- library/ZendX/Functions/Gid.php
+++ library/ZendX/Functions/Gid.php
@@ -9,16 +9,16 @@ class Gid {
         $this->_string = $this->_string ? $this->_string : 'kjasdofguas0rtlkwjer0q8r9udg9082q40trusqoidufg99a8syerqy93rqwydfy872q8734r59uqwefglakgt9w8e5ytkjdofgu0sd';
     }
     
-//    public function getId() {
-////        return @time() . substr(str_shuffle($this->_string), 0, 12);
-//        return @time() . str_shuffle(substr(str_shuffle($this->_string), 0, 12));
-//    }
     public function getId() {
-        $timestamp = time();
-        $randomChars = $this->random(9); //
-        $randomId = $timestamp . $randomChars . mt_rand(100, 999); // Thêm một số ngẫu nhiên từ 100 đến 999
-        return $randomId;
+//        return @time() . substr(str_shuffle($this->_string), 0, 12);
+        return @time() . substr(str_shuffle(str_shuffle($this->_string)), 0, 12);
     }
+//    public function getId() {
+//        $timestamp = time();
+//        $randomChars = $this->random(9); //
+//        $randomId = $timestamp . $randomChars . mt_rand(100, 999); // Thêm một số ngẫu nhiên từ 100 đến 999
+//        return $randomId;
+//    }
     
     public function random($length = 10) {
         return substr(str_shuffle($this->_string), 0, $length);
