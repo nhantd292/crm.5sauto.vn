ee30c9695ec931838d65de24f88c8f08e21bb4e8
nhantd292 <86462622+nhantd292@users.noreply.github.com>
2024-08-02 15:49:31 +0700

    bỏ điều kiện khi chia chi phí ads

--- module/Admin/src/Admin/Model/MarketingAdsTable.php
+++ module/Admin/src/Admin/Model/MarketingAdsTable.php
@@ -224,7 +224,7 @@ class MarketingAdsTable extends DefaultTable {
 	        );
 
 	        $record = $this->tableGateway->insert($data);
-	        if($record){
+//	        if($record){
 	            # cập nhật chi phí mkt cho liên hệ
                 $from_date          = $data['from_date'];
                 $to_date            = $data['to_date'];
@@ -255,7 +255,7 @@ class MarketingAdsTable extends DefaultTable {
                 $sql_update = "UPDATE ".TABLE_CONTACT." SET cost_ads = ".$cost_ads." WHERE date >= '".$firstDayOfMonth."' 
                 AND date <= '".$lastDayOfMonth." 23:59:59' AND marketer_id ='".$marketer_id."'";
                 $this->tableGateway->getAdapter()->driver->getConnection()->execute($sql_update);
-	        }
+//	        }
             return $id;
 	    }
 
